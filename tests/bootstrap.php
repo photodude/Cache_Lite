caa111e29c35d2a176ce591140894410e8ef8f99
Walt Sorensen <photodude@users.noreply.github.com>
2016-06-30 19:38:22 -0600

    try reporting all

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 if (version_compare(PHP_VERSION, '5.4', '>=')) {
-    error_reporting(E_ALL ^ E_STRICT);
+    error_reporting(E_ALL);
 } else if (version_compare(PHP_VERSION, '5.3', '>=')) {
-    error_reporting(E_ALL & ~E_STRICT);
+    error_reporting(E_ALL);
 }
